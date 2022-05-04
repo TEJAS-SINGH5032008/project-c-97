831c4aa43238fa18568d43f23060d9590166ba29
TEJAS-SINGH5032008 <86147739+TEJAS-SINGH5032008@users.noreply.github.com>
2022-05-04 17:51:26 +0530

    Add files via upload

---
+++ GuessingGame.py
@@ -0,0 +1,6 @@
+while chances < 5:
+    if guess  == number:
+        print ("Congratulations YOU WON !!!")
+        break
+        if not chances < 5:
+            print("YOU LOSE !!! The number is", number)
